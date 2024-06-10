dd314f56fb7f8b0c55bd8a7c58acd7106fb7967d
Luka Ribar <lukar@graphcore.ai>
2024-06-10 12:59:56 +0000

    Fixes

--- examples/sparq/sparq.cpp
+++ examples/sparq/sparq.cpp
@@ -5,15 +5,16 @@
 #include <random>
 #include <vector>
 #include <chrono>
+#include <cassert>
 
 using P = std::pair<int, float>;
 
-std::vector<P> topk(const float *x, int size, int k)
+std::vector<P> topk(const float *x, int size, int k, bool use_abs)
 {
     // Create a vector of indices and absolute values
     std::vector<P> x_idxs;
     for (int i = 0; i < size; i++)
-        x_idxs.emplace_back(i, std::abs(x[i]));
+        x_idxs.emplace_back(i, use_abs ? std::abs(x[i]) : x[i]);
 
     // Sort the vector based on abs values
     std::sort(x_idxs.begin(), x_idxs.end(), [](P a, P b)
@@ -25,12 +26,12 @@ std::vector<P> topk(const float *x, int size, int k)
     return x_idxs;
 }
 
-std::vector<P> topk_fast(const float *x, int size, int k)
+std::vector<P> topk_fast(const float *x, int size, int k, bool use_abs)
 {
     // Create a vector of indices and absolute values
     std::vector<P> x_idxs;
     for (int i = 0; i < size; i++)
-        x_idxs.emplace_back(i, std::abs(x[i]));
+        x_idxs.emplace_back(i, use_abs ? std::abs(x[i]) : x[i]);
 
     if (k >= size)
     {
@@ -49,7 +50,7 @@ std::vector<float> step1(const float *q, const float *K, int seq_len, int head_d
     // Output vector
     std::vector<float> out(seq_len, 0.0);
 
-    std::vector<P> idx = topk_fast(q, head_dim, k);
+    std::vector<P> idx = topk_fast(q, head_dim, k, true);
     for (int i = 0; i < seq_len; i++)
     {
         for (P p : idx)
@@ -68,7 +69,7 @@ std::vector<float> step1_t(const float *q, const float *K_t, int seq_len, int he
     // Output vector
     std::vector<float> out(seq_len, 0.0);
 
-    std::vector<P> idx = topk_fast(q, head_dim, k);
+    std::vector<P> idx = topk_fast(q, head_dim, k, true);
 
     for (P p : idx)
     {
@@ -96,14 +97,22 @@ void softmax(std::vector<float> &x)
         x[i] /= tot;
 }
 
-void sparq(const float *q, const float *K, const float *V,
+void sparq(const float *q, const float *K, const float *K_t, const float *V,
            int seq_len, int head_dim, int k1, int k2, float *out)
 {
-    // Step 1 - Probably requires K^T (no softmax for now)
-    std::vector<float> s_hat = step1_t(q, K, seq_len, head_dim, k1);
+    // Step 1
+    std::vector<float> s_hat;
+    if (K_t == nullptr)
+    {
+        s_hat = step1(q, K, seq_len, head_dim, k1);
+    }
+    else
+    {
+        s_hat = step1_t(q, K_t, seq_len, head_dim, k1);
+    }
 
     // Find top-k2 approximate scores
-    std::vector<P> topk_out = topk_fast(s_hat.data(), s_hat.size(), k2);
+    std::vector<P> topk_out = topk_fast(s_hat.data(), s_hat.size(), k2, false);
 
     // Calculate scores for top-k2, s -- (k2, )
     std::vector<float> s(k2, 0.0);
@@ -118,6 +127,12 @@ void sparq(const float *q, const float *K, const float *V,
     }
     softmax(s);
 
+    // Initialise output tensor (maybe unnecessary?)
+    for (int i = 0; i < head_dim; i ++)
+    {
+        out[i] = 0;
+    }
+
     // Perform weighted sum of values
     for (int i = 0; i < k2; i++)
     {
@@ -130,7 +145,66 @@ void sparq(const float *q, const float *K, const float *V,
     }
 }
 
-void test_step_1(bool use_transposed)
+void test_step_1()
+{
+    // head_dim = 4, seq_len = 3
+    std::vector<float> q{-5.0, 1.0, 3.0, -2.0};
+    std::vector<float> K{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    std::vector<float> K_t{1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12};
+
+    std::vector<float> out1 = step1(q.data(), K.data(), 3, 4, 2);
+    std::vector<float> out2 = step1_t(q.data(), K_t.data(), 3, 4, 2);
+
+    std::vector<float> expected{4, -4, -12};
+    assert(out1 == expected);
+    assert(out2 == expected);
+}
+
+void test_softmax()
+{
+    std::vector<float> x1{10, 10, 10, 10};
+    std::vector<float> x2{1, 2, 3, 4};
+    softmax(x1);
+    softmax(x2);
+
+    std::vector<float> expected1{0.25, 0.25, 0.25, 0.25};
+    std::vector<float> expected2{0.0321, 0.0871, 0.2369, 0.6439};
+
+    for (size_t i = 0; i < x1.size(); i++)
+    {
+        float tol = 0.001;
+        assert(std::abs(x1[i] - expected1[i]) < tol);
+        assert(std::abs(x2[i] - expected2[i]) < tol);
+    }
+}
+
+void test_sparq()
+{
+    // seq_len = 4, head_size = 2
+    // k1 = 1, k2 = 2
+    std::vector<float> q{1.0, -0.5};
+    std::vector<float> K{1, 0, 4, -10, 2, 5, -3, 10};
+    std::vector<float> K_t{1, 4, 2, -3, 0, -10, 50, 10};
+    std::vector<float> V{1, 2, 3, 4, 5, 6, 7, 8};
+
+    // After step 1, s_hat = {1, 4, 2, -3}
+    // => idxs = {1, 2} (not 3!)
+    std::vector<float> s{(4 + 5) / std::sqrt(2.f), (2 - 2.5f) / std::sqrt(2.f)};
+    softmax(s);
+    float out[2], out_t[2], expected[2];
+    expected[0] = s[0] * V[2] + s[1] * V[4];
+    expected[1] = s[0] * V[3] + s[1] * V[5];
+    sparq(q.data(), K.data(), nullptr, V.data(), 4, 2, 1, 2, out);
+    sparq(q.data(), K.data(), K_t.data(), V.data(), 4, 2, 1, 2, out_t);
+    for (int i = 0; i < 2; i++)
+    {
+        float tol = 0.001;
+        assert(std::abs(out[i] - expected[i]) < tol);
+        assert(std::abs(out_t[i] - expected[i]) < tol);
+    }
+}
+
+void benchmark_step_1(bool use_transposed)
 {
     // Tensor sizes
     const int head_dim{128};
@@ -142,14 +216,14 @@ void test_step_1(bool use_transposed)
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::normal_distribution<float> dis(0.0, 10.0);
+    std::normal_distribution<float> dis(0.0, 5.0);
     for (int i = 0; i < head_dim; i++)
         q[i] = dis(gen);
 
     for (int i = 0; i < head_dim * seq_len; i++)
         K[i] = dis(gen);
 
-    int k1 = 128; // rank
+    int k1 = 16; // rank
 
     using clock = std::chrono::system_clock;
     // Step 1
@@ -169,7 +243,7 @@ void test_step_1(bool use_transposed)
               << std::endl;
 }
 
-void test_sparq()
+void benchmark_sparq()
 {
     // Tensor sizes
     const int head_dim{128};
@@ -183,7 +257,7 @@ void test_sparq()
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::normal_distribution<float> dis(0.0, 10.0);
+    std::normal_distribution<float> dis(0.0, 5.0);
     for (int i = 0; i < head_dim; i++)
         q[i] = dis(gen);
 
@@ -193,8 +267,8 @@ void test_sparq()
         V[i] = dis(gen);
     }
 
-    int k1 = head_dim; // rank
-    int k2 = 1;
+    int k1 = head_dim;
+    int k2 = seq_len;
 
     using clock = std::chrono::system_clock;
     // Step 1
@@ -203,7 +277,7 @@ void test_sparq()
     size += 2 * k2 * head_dim * sizeof(K.front());
 
     auto t0 = clock::now();
-    sparq(q.data(), K.data(), V.data(), seq_len, head_dim, k1, k2, out.data());
+    sparq(q.data(), K.data(), K.data(), V.data(), seq_len, head_dim, k1, k2, out.data());
     auto duration = clock::now() - t0;
     auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(duration).count();
     std::cerr << "  Size: " << size / 1e9 << " GB"               //
@@ -214,6 +288,6 @@ void test_sparq()
 
 int main()
 {
-    test_step_1(true);
+    benchmark_step_1(true);
     return 0;
 }
