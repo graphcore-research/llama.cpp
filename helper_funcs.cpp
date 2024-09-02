7e3eb44139ebc7d765f18e1f48e90082f55aff42
Luka Ribar <lukar@graphcore.ai>
2024-09-02 14:25:44 +0100

    Remove print_tensor_values function

--- helper_funcs.cpp
+++ helper_funcs.cpp
@@ -21,21 +21,6 @@ void print_tensor_structure(const ggml_tensor *t, std::string name) {
     );
 }
 
-void print_tensor_values(const ggml_tensor *t) {
-    for (int i3 = 0; i3 < t->ne[3]; i3++) {
-        for (int i2 = 0; i2 < t->ne[2]; i2++) {
-            for (int i1 = 0; i1 < t->ne[1]; i1++) {
-                for (int i0 = 0; i0 < 100; i0++) {
-                    printf("%f ", ggml_get_f32_nd(t, i0, i1, i2, i3));
-                }
-                printf("\n");
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-}
-
 void print_contiguity(const ggml_tensor *t, std::string name) {
     printf("%s is ", name.c_str());
     printf(ggml_is_contiguous(t) ? "contiguous\n" : "not contiguous\n");
--- helper_funcs.h
+++ helper_funcs.h
@@ -6,5 +6,4 @@
 #include "ggml.h"
 
 void print_tensor_structure(const ggml_tensor *t, std::string name);
-void print_tensor_values(const ggml_tensor *t);
 void print_contiguity(const ggml_tensor *t, std::string name);
